b6a631593e20eb2d240e7ea6a3d2be1098bc199f
Adnan Öztürk <adnan.oeztuerk@mhp.com>
2021-12-23 20:43:45 +0100

    Updates

---
+++ precache-manifest.74f12d6ceee30173c8e0430e6cd4861b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9293ae64381a19fb2aa8a7b22c2b8fa",
+    "url": "/AO19/seinfeld-soundboard/index.html"
+  },
+  {
+    "revision": "0cab38f57bf87b3e8166",
+    "url": "/AO19/seinfeld-soundboard/static/css/main.2d4e3cc1.chunk.css"
+  },
+  {
+    "revision": "9f3bc12c4a9b322cf005",
+    "url": "/AO19/seinfeld-soundboard/static/js/2.fd5f7f08.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/AO19/seinfeld-soundboard/static/js/2.fd5f7f08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cab38f57bf87b3e8166",
+    "url": "/AO19/seinfeld-soundboard/static/js/main.73a54fc1.chunk.js"
+  },
+  {
+    "revision": "b11952fc0f33271c5a05",
+    "url": "/AO19/seinfeld-soundboard/static/js/runtime-main.a2904be6.js"
+  },
+  {
+    "revision": "61c6ab55b3e8cd61fc2c8523e9424551",
+    "url": "/AO19/seinfeld-soundboard/static/media/pattern.61c6ab55.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AO19/seinfeld-soundboard/precache-manifest.cd002c9a8ccb7f1fc38a055a1663dbdb.js"
+  "/AO19/seinfeld-soundboard/precache-manifest.74f12d6ceee30173c8e0430e6cd4861b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/AO19/seinfeld-soundboard/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AO19/seinfeld-soundboard/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
